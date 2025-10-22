8d8e571a9b8ca5c8845e258d346111e4f0e3dbe7
aeni36966-hue <aeni36966@gmail.com>
2025-10-22 14:37:05 +0700

    Add files via upload

---
+++ classs.py
@@ -0,0 +1,25 @@
+class Sekolah:
+  pass
+  
+sekolah1 = Sekolah()
+sekolah1.nama = "SMK PGRI 35 SOLOKANJERUK"
+sekolah1.alamat = "JL RHO KOSASIH NO 90 CIBODAS"
+sekolah2 = Sekolah()
+sekolah2.nama = "SMKN 9 BANDUNG"
+sekolah2.alamat = "JL Soekarno Hatta NO 10"
+
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat)
+
+
+class Siswa:
+  pass
+  
+siswa1 = Siswa()
+siswa2 = Siswa()
+siswa3 = Siswa()
+  
+print(type(siswa1))
+print(type(siswa2))
---
+++ cltugas1_aeni.py
@@ -0,0 +1,33 @@
+class Alatmusik:
+     pass
+  
+gitar = Alatmusik()
+piano = Alatmusik()
+
+
+gitar = Alatmusik()
+gitar.nama = "gitar bass"
+gitar.jenis = "gitar listrik"
+gitar.warna = "coklat"
+gitar.tahun_rilis = 1951
+gitar.harga = "2.000.000"
+
+
+piano = Alatmusik()
+piano.nama = "Yamaha Grand Piano C3"
+piano.jenis = "Piano Akustik"
+piano.warna = "Hitam Glossy"
+piano.tahun_rilis = 2021
+piano.harga = "180000000"
+
+print(gitar.nama)
+print(gitar.jenis)
+print(gitar.warna)
+print(gitar.tahun_rilis)
+print(gitar.harga)
+
+print(piano.nama)
+print(piano.jenis)
+print(piano.warna)
+print(piano.tahun_rilis)
+print(piano.harga)
